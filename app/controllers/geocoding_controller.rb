e8e4fdbb8f362ef6f5f3f4029eed554666cbb45c
nrbooker <nrbooker@chicagobooth.edu>
2015-07-21 20:49:07 -0500

    Geocoding Controller
    Step 1

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'JSON'
 
 class GeocodingController < ApplicationController
   def street_to_coords_form
@@ -18,10 +19,14 @@ class GeocodingController < ApplicationController
     # ==========================================================================
 
 
+url = "http://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address
+street_data = open(url).read
+parsed_data = JSON.parse(street_data)
+latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+@latitude = latitude
+@longitude = longitude
 
     render("street_to_coords.html.erb")
   end
