425347852f9ba0822327f8ceeea4d81e680b406b
nrbooker <nrbooker@chicagobooth.edu>
2015-07-21 21:51:15 -0500

    Updated Weather
    Updated Weather -

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -18,20 +18,25 @@ class ForecastController < ApplicationController
 
 
 url = "https://api.forecast.io/forecast/aab2143d604a9b6130ea6546d66bc846/#{@lat},#{@lng}"
-wet_data = open(url).read
-parsedd_data = JSON.parse(wet_data)
-current_temperature = parsedd_data["currently"]["temperature"]
-current_summary = parsedd_data["currently"]["summary"]
-summary_of_next_sixty_minutes = parsedd_data["minutely"]["summary"]
-summary_of_next_several_hours = parsedd_data["hourly"]["summary"]
-summary_of_next_several_days = parsedd_data["daily"]["summary"]
+wt_data = open(url).read
+parsed_data = JSON.parse(wt_data)
+current_temperature = parsed_data["currently"]["temperature"]
+current_summary = parsed_data["currently"]["summary"]
+summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
+summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+summary_of_next_several_days = parsed_data["daily"]["summary"]
 
 
     @current_temperature = current_temperature
+
     @current_summary = current_summary
+
     @summary_of_next_sixty_minutes = summary_of_next_sixty_minutes
+
     @summary_of_next_several_hours = summary_of_next_several_hours
+
     @summary_of_next_several_days = summary_of_next_several_days
+
     render("coords_to_weather.html.erb")
   end
 end
